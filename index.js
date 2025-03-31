fb941406544b1685f2e772c45e223d5f9d2df9bd
sagar <sagardabas2019@gmail.com>
2025-04-01 01:17:32 +0530

    completed the game

--- index.js
+++ index.js
@@ -1,25 +1,64 @@
-let firstCard = 10;
-let secondCard = 11;  
-let sum = firstCard + secondCard;  
+
+let player = {
+    name: "Sagar",
+    chips: 100
+};
+
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
 let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
-console.log(messageEl)
+console.log(messageEl);
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
 
-function startGame() {
-    if (sum <= 20) {
-        message = "Do you want to draw another card?"
-        messageEl.textContent = message;
-    } else if (sum === 21) {
-        message = "BlackJack!"
-        messageEl.textContent = message;
-    } else {
-        message = "You are out of the game!"
-        messageEl.textContent = message;
-    }
+playerEl.textContent = player.name + ": $" + player.chips;
 
-    
+function randomCard() {
+  return Math.floor(Math.random() * 10) + 1;
 }
 
+function startGame() {
+  isAlive = true;
+  firstCard = randomCard();
+  secondCard = randomCard();
+  sum = firstCard + secondCard;
+  cards = [firstCard, secondCard];
 
+  renderGame();
+}
+
+function renderGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw another card?";
+    isAlive = true;
+    hasBlackJack = false;
+  } else if (sum === 21) {
+    message = "BlackJack!";
+    isAlive = false;
+    hasBlackJack = true;
+  } else {
+    message = "You are out of the game!";
+    isAlive = false;
+    hasBlackJack = false;
+  }
+
+  messageEl.textContent = message;
+  sumEl.textContent = "sum: " + sum;
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+}
+
+function newCard() {
+  if (isAlive === true && hasBlackJack === false) {
+    let card = randomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
+}
